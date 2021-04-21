1460012bdda7f935e285ee0f997dd5c385a312b0
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-21 09:19:56 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,16 @@
 export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+  state = {
+    items: [],
+  },
+  action,
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return {
+        ...state,
+        items: state.items.concat(state.items.length + 1),
+      };
+    default:
+      return state;
+  }
 }
